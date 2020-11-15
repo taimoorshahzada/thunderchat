e1ad131c3448a6b114bcbbf7f12c9b2e70a38c6d
taimoorshahzada <66862925+taimoorshahzada@users.noreply.github.com>
2020-11-15 14:49:00 +0500

    Add files via upload

---
+++ client.js
@@ -0,0 +1,40 @@
+
+const socket = io('https://git.heroku.com/thunderchat-app.git');
+
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('msgInp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('iphone_sms_original.mp3');
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+
+    }
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+
+const name = prompt("Enter your name to Join: ");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name =>{
+    append(` ${name} joined the chat`, 'right');
+})
+socket.on('receive', data =>{
+    append(` ${data.name}: ${data.message}`, 'left');
+})
+socket.on('left', name =>{
+    append(` ${name} left the chat`, 'right');
+})
